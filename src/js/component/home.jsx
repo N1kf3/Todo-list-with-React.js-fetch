bf429b5843d045a06686898f0b24312e151ab548
N1kf3 <wilfredo.andrade92@gmail.com>
2023-02-10 18:56:34 +0000

    primera version

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import List from "./List.jsx"
-import Card from "./Card.jsx"
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 
 
